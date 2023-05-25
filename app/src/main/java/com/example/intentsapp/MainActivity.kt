cfb772ffc020514cbd9559116547b580bef562b2
Alphonce08 <alphoncerotich30@gmail.com>
2023-05-25 15:54:36 +0300

    intenet app woking

---
+++ app/src/main/java/com/example/intentsapp/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.intentsapp
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btnSms:Button
+    lateinit var btnEmail:Button
+    lateinit var btnCamera:Button
+    lateinit var btnShare:Button
+    lateinit var btnMpesa:Button
+    lateinit var btnCall:Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnSms = findViewById(R.id.mbtSms)
+        btnEmail = findViewById(R.id.mbtemail)
+        btnCamera = findViewById(R.id.mbtCamera)
+        btnShare = findViewById(R.id.mbtShare)
+        btnMpesa = findViewById(R.id.mbtMpesa)
+        btnCall = findViewById(R.id.mbtCall)
+
+        btnSms.setOnClickListener {
+            val uri = Uri.parse("smsto:0708477938")
+            val intent = Intent(Intent.ACTION_SENDTO, uri)
+            intent.putExtra("sms_body", "Hello, i'am Alphonce")
+            startActivity(intent)
+
+        }
+        btnEmail.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "king10@gmail.com", null))
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject")
+            emailIntent.putExtra(Intent.EXTRA_TEXT, "Dear sir.....")
+            startActivity(Intent.createChooser(emailIntent, "Send email..."))
+
+        }
+        btnCamera.setOnClickListener {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(takePictureIntent, 1)
+        }
+        btnShare.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, "Hey, download this app!")
+            startActivity(shareIntent)
+        }
+        btnMpesa.setOnClickListener {
+            val simToolKitLaunchIntent =
+                applicationContext.packageManager.getLaunchIntentForPackage("com.android.stk")
+            simToolKitLaunchIntent?.let { startActivity(it) }
+        }
+        btnCall.setOnClickListener {
+            val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "+918511812660"))
+            if (ContextCompat.checkSelfPermission(this@MainActivity, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this@MainActivity, arrayOf<String>(Manifest.permission.CALL_PHONE), 1)
+            } else {
+                startActivity(intent)
+            }
+        }
+    }
+}
